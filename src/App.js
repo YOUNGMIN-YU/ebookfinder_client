788e21b5c5a11367470137f43b26c5f99345b684
YOUNGMIN-YU <kooonym@gmail.com>
2024-02-21 20:19:27 +0900

    feat: AppHeader 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './styles/App.css';
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
+import AppHeader from './components/AppHeader';
 
 function App() {
   return (
     <div className="App">
+      <AppHeader />
       <div className="Main">
         <Routes>
           <Route path="/" element={<Main />}/>
