cb5b3547ae8aa2573015da5e2bdf17b7cdb2dcb3
YOUNGMIN-YU <kooonym@gmail.com>
2024-03-19 17:10:05 +0900

    feat: reat-query 기능 추가

--- src/index.js
+++ src/index.js
@@ -2,11 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// RQ 클라이언트 선언 및 옵션설정
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      useErrorBoundary: true,
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+    mutations: {
+      useErrorBoundary: true,
+    },
+  },
+})
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen='true' />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
