d956a7f1e0882d66f1118230126d190756805d43
YOUNGMIN-YU <kooonym@gmail.com>
2024-04-19 11:09:30 +0900

    feat: Tour, Step 상태 추가 및 API 환경 변수 설정

--- src/store/useBookStore.js
+++ src/store/useBookStore.js
@@ -10,6 +10,8 @@ const useBookStore = create((set) => ({
     elibSortBy: 'elibName',
     ebookSortBy: 'ebookDate',
     elibId: 0,
+    tourOpen: false,
+
     actions: {
         addBooks: (data) => set((state) => ({ books: [...state.books, ...data] })),
 
@@ -22,6 +24,8 @@ const useBookStore = create((set) => ({
         setElibId: (val) => set({ elibId: val }),
 
         removeAllBooks: () => set({ books: [], searchedBooks: [] }),
+
+        setTourOpen: (val) => set({ tourOpen: val }),
     }
 }))
 
@@ -31,38 +35,39 @@ export const useSearchElibKeyword = () => useBookStore((state) => state.searchEl
 export const useElibSortBy = () => useBookStore((state) => state.elibSortBy);
 export const useEbookSortBy = () => useBookStore((state) => state.ebookSortBy);
 export const useElibId = () => useBookStore((state) => state.elibId);
+export const useTourOpen = () => useBookStore((state) => state.tourOpen);
+export const useSteps = () => useBookStore((state) => state.steps);
 
 export const useBookStoreActions = () => useBookStore((state) => state.actions);
 
-
-const fetchBooks = async (elibId, elibSortBy, { pageParam })  => {
+const fetchBooks = async (elibId, elibSortBy, { pageParam }) => {
     if (elibId === 0) {
         return; //!꼭 끝내줘야 함/ API통신 전이기에 위로 배치
     }
 
-    const response = await axios.get(`http://localhost:8081`, { params: { sort: elibSortBy }});
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/elibs/${elibId}/ebooks?page=${pageParam}`, { params: { sort: elibSortBy } });
     return response.data;
 }
 
-const fetchSearchedBooks = async (searchKeyword, elibSortBy, { pageParam })  => {
-    if (searchKeyword === '' || searchKeyword === null ) {
+const fetchSearchedBooks = async (searchKeyword, elibSortBy, { pageParam }) => {
+    if (searchKeyword === '' || searchKeyword === null) {
         return; //!꼭 끝내줘야 함/ API통신 전이기에 위로 배치
     }
-    
-    const response = await axios.get('http://localhost:8081', { 
-        params: { keyword: searchKeyword, sort: elibSortBy, page: pageParam}
+
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/ebooks`, {
+        params: { keyword: searchKeyword, sort: elibSortBy, page: pageParam }
     });
     return response.data;
 }
 
-const fetchElibs = async (elibName, elibSortBy)  => {
-    const response = await axios.get('http://localhost:8081',{
+const fetchElibs = async (elibName, elibSortBy) => {
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/elibs`, {
         params: { elibName: elibName, sort: elibSortBy }
     });
     return response.data;
 }
 
-export const useAddBooks = () => {
+export const useElibBooks = () => {
     const elibId = useElibId();
     const ebookSortBy = useEbookSortBy();
 
@@ -74,7 +79,7 @@ export const useAddBooks = () => {
             const nextPage = allPage.length + 1;
             const totalItems = lastPage?.numFound;
             const totalPages = Math.ceil(totalItems / 20);
-            
+
             return lastPage?.data.length === 0 || totalPages === allPage.length ? undefined : nextPage;
         },
         select: (data) => ({
@@ -83,7 +88,7 @@ export const useAddBooks = () => {
             meta: {
                 numFound: data?.pages[0].numFound,
             }
-          }),
+        }),
     })
 
     return { data, status, error, fetchNextPage, hasNextPage };
@@ -101,18 +106,17 @@ export const useSearchedBooks = () => {
             const nextPage = allPage.length + 1;
             const totalItems = lastPage?.numFound;
             const totalPages = Math.ceil(totalItems / 20);
-            
+
             return lastPage?.data.length === 0 || totalPages === allPage.length ? undefined : nextPage;
         },
-        select: (data) => (
-            console.log("selectedData: ",data),{
-            pages: data?.pages.flatMap((page) => page.data),
-            pageParams: data.pageParams,
-            meta: {
-                numFound: data?.pages[0].numFound,
-                searchKeyword: searchKeyword,
-            }
-        })
+        select: (data) => ({
+                pages: data?.pages.flatMap((page) => page.data),
+                pageParams: data.pageParams,
+                meta: {
+                    numFound: data?.pages[0].numFound,
+                    searchKeyword: searchKeyword,
+                }
+            })
     });
 
     return { data, status, error, fetchNextPage, hasNextPage, };
@@ -126,7 +130,7 @@ export const useSearchedElibs = () => {
         queryKey: ['searchedElibs', searchElibKeyword],
         queryFn: ({ queryKey }) => fetchElibs(queryKey[1], elibSortBy),
         select: (data) => ({
-            datas:data?.data?.flatMap((data) => data),
+            datas: data?.data?.flatMap((data) => data),
             meta: {
                 numFound: data?.numFound,
                 searchElibKeyword: searchElibKeyword,
